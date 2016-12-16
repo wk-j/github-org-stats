25a20f971fc0a1f87d918e5f71028d92d401aa82
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-12-16 23:52:10 +0700

    Test connecting to Github =^.^=

--- GitHubOrgStats.Tests/ApiSpec.cs
+++ GitHubOrgStats.Tests/ApiSpec.cs
@@ -1,7 +1,22 @@
 ﻿using System;
+using Octokit;
+using FluentAssertions;
+using NUnit.Framework;
+
 namespace GitHubOrgStats.Tests {
 	public class ApiSpec {
 		public ApiSpec() {
+		}
+
+		[Test]
+		public async void ShouldConnectToGithub() {
+			var app = "MyApp";
+			var github = new GitHubClient(new ProductHeaderValue(app));
+
+			var user = await github.User.Get("wk-j");
+			var repo = user.PublicRepos;
+
+			repo.Should().BeGreaterThan(10);
 		}
 	}
 }
